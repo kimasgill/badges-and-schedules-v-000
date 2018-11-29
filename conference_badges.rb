587b3847a97c26bf9482b0410997a10ce8026e2c
Kimberly Asgill <kimberlyasgill@gmail.com>
2018-11-29 05:46:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+attendees = ["Edsgar", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+# def batch_badge_creator(name)
+# badges = []
+# attendees.each do |i|
+#   badges << badge_maker(name)
+#   badges
+# end
+# end
+
+def batch_badge_creator(name)
+  attendees.each do |i|
+    attendees << badge_maker(name)
+  end
+end
